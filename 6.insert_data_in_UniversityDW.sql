70e7d9e39324b0ae4eb5f2a52d1f6e7d78c77c6c
Mbali Msweli <mbalimsweli.nongalo@gmail.com>
2025-12-02 15:51:02 +0200

    insert data

--- 6.insert_data_in_UniversityDW.sql
+++ 6.insert_data_in_UniversityDW.sql
@@ -1,3 +1,5 @@
+
+ -----Inserting data in DW_Year1Modules Table----
 INSERT INTO [UniversityDW].[dbo].[DW_Year1Modules] (
 	ModuleCode ,
 	ModuleName ,
@@ -16,3 +18,43 @@ WHERE NOT EXISTS (
     SELECT 1
     FROM[UniversityDW].[dbo].[DW_Year1Modules] AS DWH
     WHERE DWH.DW_ID = StagingTable.ID
+
+    -----Inserting data in DW_Year2Modules Table----
+INSERT INTO [UniversityDW].[dbo].[DW_Year2Modules] (
+	ModuleCode ,
+	ModuleName ,
+	Credits ,
+	Category,
+	LoadDate)
+SELECT
+   S.[ModuleCode],
+   S.[ModuleName],
+   S.[Credits],
+   S.[Category],
+   S.[DateInserted]
+FROM
+    [UniversityStagingDB].[dbo].[Year2Modules] AS S
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM[UniversityDW].[dbo].[DW_Year2Modules] AS DWH
+    WHERE DWH.DW_ID = S.ID
+
+ -----Inserting data in DW_Year3Modules Table----
+INSERT INTO [UniversityDW].[dbo].[DW_Year3Modules] (
+	ModuleCode ,
+	ModuleName ,
+	Credits ,
+	Category,
+	LoadDate)
+SELECT
+   S.[ModuleCode],
+   S.[ModuleName],
+   S.[Credits],
+   S.[Category],
+   S.[DateInserted]
+FROM
+    [UniversityStagingDB].[dbo].[Year3Modules] AS S
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM[UniversityDW].[dbo].[DW_Year3Modules] AS DWH
+    WHERE DWH.DW_ID = S.ID
