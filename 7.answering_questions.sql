7670a692892b1fcafce2a807c09ea650673b43ed
Mbali Msweli <mbalimsweli.nongalo@gmail.com>
2025-12-04 11:36:28 +0200

    answering questions

---
+++ 7.answering_questions.sql
@@ -0,0 +1,68 @@
+----How many modules exist per year.
+
+SELECT COUNT(DW_ID) AS ModulesYr1 FROM [UniversityDW].[dbo].[DW_Year1Modules];
+
+SELECT COUNT(DW_ID) AS  ModulesYr2 FROM [UniversityDW].[dbo].[DW_Year2Modules];
+
+SELECT COUNT(DW_ID) AS  ModulesYr3 FROM [UniversityDW].[dbo].[DW_Year3Modules];
+
+
+----Total credits per year.
+SELECT SUM(Credits) AS TotalCreditsYr1 FROM [UniversityDW].[dbo].[DW_Year1Modules];
+
+SELECT SUM(Credits) AS  TotalCreditsYr2 FROM [UniversityDW].[dbo].[DW_Year2Modules];
+
+SELECT SUM(Credits) AS  TotalCreditsYr3 FROM [UniversityDW].[dbo].[DW_Year3Modules];
+
+
+---Module category breakdown (Fundamental, Core, Elective).
+
+SELECT Category,COUNT(ModuleCode) as NumberOfModulesYr1
+FROM [UniversityDW].[dbo].[DW_Year1Modules]
+GROUP BY Category;
+
+SELECT Category,COUNT(ModuleCode) as NumberOfModulesYr2
+FROM [UniversityDW].[dbo].[DW_Year2Modules]
+GROUP BY Category;
+
+SELECT Category,COUNT(ModuleCode) as NumberOfModulesYr3
+FROM [UniversityDW].[dbo].[DW_Year3Modules]
+GROUP BY Category;
+
+
+-----Find duplicate module codes.
+
+SELECT
+    ModuleCode,
+    COUNT(ModuleCode) AS DuplicatesYr1
+FROM
+   [UniversityDW].[dbo].[DW_Year1Modules]
+GROUP BY
+    ModuleCode
+HAVING
+    COUNT(ModuleCode) > 1;
+
+
+SELECT
+    ModuleCode,
+    COUNT(ModuleCode) AS DuplicatesYr2
+FROM
+   [UniversityDW].[dbo].[DW_Year2Modules]
+GROUP BY
+    ModuleCode
+HAVING
+    COUNT(ModuleCode) > 1;
+
+
+SELECT
+    ModuleCode,
+    COUNT(ModuleCode) AS DuplicatesYr3
+FROM
+   [UniversityDW].[dbo].[DW_Year3Modules]
+GROUP BY
+    ModuleCode
+HAVING
+    COUNT(ModuleCode) > 1;
+
+
+----Combine all modules from all years using UNION ALL.
