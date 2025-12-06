ae53194f41931518269e982c58bf7b0805dfeb7c
Mbali Msweli <mbalimsweli.nongalo@gmail.com>
2025-12-06 06:32:27 +0200

    fix code

--- 5.create_database_UniversityDW.sql
+++ 5.create_database_UniversityDW.sql
@@ -1,5 +1,22 @@
-create database UniversityDW
+---CREATE DATABASE: UniversityDW
 
+IF NOT EXISTS(
+SELECT name
+FROM sys.databases
+WHERE name = 'UniversityDW'
+)
+BEGIN
+CREATE DATABASE UniversityDW
+END
+
+---CREATE TABLE: UniversityDW.dbo.DW_Year1Modules
+
+IF NOT EXISTS (
+    SELECT * FROM sys.objects 
+    WHERE object_id = OBJECT_ID('UniversityDW.dbo.DW_Year1Modules') 
+      AND type = 'U'
+)
+BEGIN
 CREATE TABLE DW_Year1Modules(
 	DW_ID int PRIMARY KEY IDENTITY(1,1),
 	ModuleCode varchar (50) NULL,
@@ -7,8 +24,16 @@ CREATE TABLE DW_Year1Modules(
 	Credits int NULL,
 	Category varchar(500) NULL,
 	LoadDate DATETIME2 DEFAULT CURRENT_TIMESTAMP);
+END
 
+---CREATE TABLE: UniversityDW.dbo.DW_Year2Modules
 
+IF NOT EXISTS (
+    SELECT * FROM sys.objects 
+    WHERE object_id = OBJECT_ID('UniversityDW.dbo.DW_Year2Modules') 
+      AND type = 'U'
+)
+BEGIN
 	CREATE TABLE DW_Year2Modules(
 	DW_ID int PRIMARY KEY IDENTITY(1,1),
 	ModuleCode varchar (50) NULL,
@@ -16,8 +41,16 @@ CREATE TABLE DW_Year1Modules(
 	Credits int NULL,
 	Category varchar(500) NULL,
 	LoadDate DATETIME2 DEFAULT CURRENT_TIMESTAMP);
+END
 
+---CREATE TABLE: UniversityDW.dbo.DW_Year3Modules
 
+IF NOT EXISTS (
+    SELECT * FROM sys.objects 
+    WHERE object_id = OBJECT_ID('UniversityDW.dbo.DW_Year3Modules') 
+      AND type = 'U'
+)
+BEGIN
 	CREATE TABLE DW_Year3Modules(
 	DW_ID int PRIMARY KEY IDENTITY(1,1),
 	ModuleCode varchar (50) NULL,
@@ -25,4 +58,4 @@ CREATE TABLE DW_Year1Modules(
 	Credits int NULL,
 	Category varchar(500) NULL,
 	LoadDate DATETIME2 DEFAULT CURRENT_TIMESTAMP);
-
+END
