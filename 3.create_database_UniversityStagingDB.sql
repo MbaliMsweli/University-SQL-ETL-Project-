3e270bd0ff1e16561a23ede55c740b9cab6da50e
Mbali Msweli <mbalimsweli.nongalo@gmail.com>
2025-12-06 06:10:58 +0200

    created database and tables

--- 3.create_database_UniversityStagingDB.sql
+++ 3.create_database_UniversityStagingDB.sql
@@ -1,5 +1,21 @@
-create database UniversityStagingDB
+---CREATE DATABASE: UniversityStagingDB
 
+USE [master]
+IF NOT EXISTS (
+SELECT name FROM sys.databases 
+WHERE name = 'UniversityStagingDB')
+BEGIN
+    CREATE DATABASE UniversityStagingDB;
+END
+
+
+-- CREATE TABLE: Year1Modules
+
+IF NOT EXISTS (
+    SELECT * FROM sys.objects 
+    WHERE object_id = OBJECT_ID('UniversityStagingDB.dbo.Year1Modules') 
+      AND type = 'U'
+)
 CREATE TABLE Year1Modules(
 	ID int PRIMARY KEY IDENTITY(1,1),
 	ModuleCode varchar (50) NULL,
@@ -8,6 +24,14 @@ CREATE TABLE Year1Modules(
 	Category varchar(500) NULL,
 	DateInserted DATETIME2 DEFAULT CURRENT_TIMESTAMP);
 
+-- CREATE TABLE: Year1Modules
+
+IF NOT EXISTS (
+    SELECT * FROM sys.objects 
+    WHERE object_id = OBJECT_ID('UniversityStagingDB.dbo.Year2Modules') 
+      AND type = 'U'
+)
+
 CREATE TABLE Year2Modules(
 	ID int PRIMARY KEY IDENTITY(1,1),
 	ModuleCode varchar (50) NULL,
@@ -15,6 +39,15 @@ CREATE TABLE Year2Modules(
 	Credits int NULL,
 	Category varchar(500) NULL,
 	DateInserted DATETIME2 DEFAULT CURRENT_TIMESTAMP);
+
+
+-- CREATE TABLE: Year3Modules
+
+IF NOT EXISTS (
+    SELECT * FROM sys.objects 
+    WHERE object_id = OBJECT_ID('UniversityStagingDB.dbo.Year3Modules') 
+      AND type = 'U'
+)
 
 CREATE TABLE Year3Modules(
 	ID int PRIMARY KEY IDENTITY(1,1),
